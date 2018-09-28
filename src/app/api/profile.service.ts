b824cc879c487f57b756bb81d6f4ec3c5e507360
ender <xuender@gmail.com>
2018-09-28 14:10:10 +0800

    fix #13

--- src/app/api/profile.service.ts
+++ src/app/api/profile.service.ts
@@ -3,6 +3,7 @@ import { AlertController, NavController } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
 
 import { URL } from './init'
+import { User } from '../users/user';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +15,19 @@ export class ProfileService {
     private alertCtrl: AlertController,
     private navCtrl: NavController,
   ) { }
+
+  get profile$() {
+    return this.http.get<any>(`${URL}/api/profile`)
+  }
+
+  save(user: User) {
+    return this.http.patch<User>(`${URL}/api/profile`, user)
+  }
+
   logout() {
     localStorage.removeItem('token')
   }
+
   login() {
     this.alertCtrl.create({
       header: '用户登录',
@@ -55,11 +66,55 @@ export class ProfileService {
       ]
     }).then(a => a.present());
   }
+
   error(msg: string) {
     this.alertCtrl.create({
       header: '错误',
       message: msg,
       buttons: ['确定']
     }).then(a => a.present())
+  }
+
+  async pass() {
+    const alert = await this.alertCtrl.create({
+      header: '修改密码',
+      subHeader: '修改当前用户登录密码',
+      inputs: [
+        {
+          name: 'old',
+          label: '旧密码',
+          type: 'password',
+          placeholder: '用户旧密码'
+        },
+        {
+          name: 'pass',
+          label: '新密码',
+          type: 'password',
+          placeholder: '用户新密码'
+        },
+        {
+          name: 'two',
+          label: '重复新密码',
+          type: 'password',
+          placeholder: '重复新密码'
+        },
+      ],
+      buttons: [
+        {
+          text: '取消',
+          role: 'cancel',
+          cssClass: 'secondary',
+        }, {
+          text: '确定',
+          handler: (v) => {
+            if (v['pass'] != v['two']) { return false }
+            this.http.patch<string>(`${URL}/api/profile/pass?old=${v['old']}&pass=${v['pass']}`, {})
+              .subscribe((t: string) => localStorage.setItem('token', t))
+            return true
+          }
+        }
+      ]
+    })
+    await alert.present();
   }
 }
--- src/app/setting/profile/profile.page.ts
+++ src/app/setting/profile/profile.page.ts
@@ -1,84 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { ModalController, AlertController, ToastController } from '@ionic/angular';
+import { Component } from '@angular/core';
+import { ModalController, ToastController } from '@ionic/angular';
 
-import { URL } from '../../api/init'
-import { User } from '../../users/user';
 import { ProfileService } from '../../api/profile.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage {
   user: any = { id: '', nick: '' }
   constructor(
     private modalCtrl: ModalController,
     private profile: ProfileService,
-    private http: HttpClient,
-    private alertCtrl: AlertController,
     private toastCtrl: ToastController,
   ) {
-    this.http.get<any>(`${URL}/api/profile`)
-      .subscribe(p => this.user = p)
+    this.profile.profile$.subscribe(p => this.user = p)
   }
 
-  ngOnInit() {
-  }
   logout() {
     this.profile.logout()
     this.modalCtrl.dismiss();
     this.profile.login()
   }
+
   cancel() {
     this.modalCtrl.dismiss();
   }
 
   async save() {
-    await this.http.patch<User>(`${URL}/api/profile`, this.user).toPromise()
-    const toast = await this.toastCtrl.create({ message: '账户信息保存成功' })
+    await this.profile.save(this.user).toPromise()
+    const toast = await this.toastCtrl.create({ message: '账户信息保存成功', duration: 2000, position: 'middle', })
     toast.present()
   }
-  async pass() {
-    const alert = await this.alertCtrl.create({
-      header: '修改密码',
-      subHeader: '修改当前用户登录密码',
-      inputs: [
-        {
-          name: 'old',
-          label: '旧密码',
-          type: 'password',
-          placeholder: '用户旧密码'
-        },
-        {
-          name: 'pass',
-          label: '新密码',
-          type: 'password',
-          placeholder: '用户新密码'
-        },
-        {
-          name: 'two',
-          label: '重复新密码',
-          type: 'password',
-          placeholder: '重复新密码'
-        },
-      ],
-      buttons: [
-        {
-          text: '取消',
-          role: 'cancel',
-          cssClass: 'secondary',
-        }, {
-          text: '确定',
-          handler: (v) => {
-            if (v['pass'] != v['two']) { return false }
-            this.http.patch<string>(`${URL}/api/profile/pass?old=${v['old']}&pass=${v['pass']}`, {})
-              .subscribe((t: string) => localStorage.setItem('token', t))
-            return true
-          }
-        }
-      ]
-    })
-    await alert.present();
+
+  pass() {
+    this.profile.pass()
   }
 }

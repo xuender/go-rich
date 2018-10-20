9e436961d0da2eea59b051ba9170e6c310ec760d
ender <xuender@gmail.com>
2018-10-20 18:10:28 +0800

    build(makefile): 彻底删除keys目录，需要时自动创建

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import "./cmd"
 
+//go:generate mkdir keys -p
 //go:generate go run $GOROOT/src/crypto/tls/generate_cert.go --host=localhost
 //go:generate mv cert.pem key.pem keys
 //go:generate go-bindata -nomemcopy -pkg keys -o ./keys/bindata.go keys/cert.pem keys/key.pem

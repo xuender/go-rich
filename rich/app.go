d40357599241cf5be4e74b06eb2b2edab3b4d72b
ender <xuender@gmail.com>
2018-10-20 18:05:01 +0800

    fix(console): 彻底删除cli类库

--- rich/app.go
+++ rich/app.go
@@ -1,23 +1,11 @@
 package rich
 
-import "github.com/urfave/cli"
-
 // App 应用信息
 type App struct {
 	Name    string `json:"name"`    // 应用名称
 	Usage   string `json:"usage"`   // 使用方法
 	Version string `json:"version"` // 版本
 	IsNew   bool   `json:"isNew"`   // 新应用
-}
-
-// NewApp 新建应用
-func NewApp(app *cli.App) *App {
-	a := App{}
-	a.Name = app.Name
-	a.Usage = app.Usage
-	a.Version = app.Version
-	a.IsNew = true
-	return &a
 }
 
 // NewAppVar 新建应用
